2b06a73fb0a8e4b937f259724e3bade7e78a800a
Your Name <you@example.com>
2025-01-27 16:37:56 +0300

    create cae v1

--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
-provider "huaweicloud" {}
-variable "environment_id" {}
-variable "application_id" {}
-variable "component_name" {}
-variable "image_url" {}
+provider "huaweicloud" {
+  project_id = "935ca239302b4794a8efbd7affc25661"
+}
 
 resource "huaweicloud_cae_component" "test" {
   environment_id = "46dc4388-0383-4f16-b5d0-81bc7e28ad93"
   application_id =  "75e7e682-d7c9-4dde-b5c4-7cd954bbdecb"
 
   metadata {
-    name = var.component_name
+    name = "test01"
 
     annotations = {
       version = "1.0.0"
@@ -22,12 +20,48 @@ resource "huaweicloud_cae_component" "test" {
 
     source {
       type = "image"
-      url  = "docker pull swr.me-east-1.myhuaweicloud.com/frank/nginx:1.19"
+      url  = "swr.me-east-1.myhuaweicloud.com/frank/nginx:1.19"
     }
 
     resource_limit {
       cpu    = "500m"
       memory = "1Gi"
     }
+  }
+  deploy_after_create = true
+}
+
+resource "huaweicloud_cae_component_configurations" "test" {
+  environment_id = "46dc4388-0383-4f16-b5d0-81bc7e28ad93"
+  application_id =  "75e7e682-d7c9-4dde-b5c4-7cd954bbdecb"
+  component_id   = huaweicloud_cae_component.test.id
+  items {
+    type = "lifecycle"
+    data = jsonencode({
+      "spec": {
+        "postStart": {
+          "exec": {
+            "command": [
+              "/bin/bash",
+              "-c",
+              "sleep",
+              "10",
+              "done",
+            ]
+          }
+        }
+      }
+    })
+  }
+  items {
+    type = "env"
+    data = jsonencode({
+      "spec": {
+        "envs": {
+            "key": "value",
+            "foo": "bar"
+        }
+      }
+    })
   }
 }

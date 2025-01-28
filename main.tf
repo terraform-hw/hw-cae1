b9ac58d2ca2d669530ae282958854a86da99b29e
cha-frank <frank.chamij@gmail.com>
2025-01-27 23:24:44 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,108 @@
 provider "huaweicloud" {
   project_id = "935ca239302b4794a8efbd7affc25661"
 }
-
+locals {
+  com1_configurations = [
+    {
+      type = "env"
+      data = jsonencode({
+        "spec" : {
+          "envs" : {
+            "key1" : "value1",
+            "key2" : "value2"
+          }
+        }
+      })
+    },
+    {
+      type = "access"
+      data = jsonencode({
+        "spec" : {
+          "items" : [
+            {
+              "type" : "ClusterIP",
+              "ports" : [
+                {
+                  "target_port" : 80,
+                  "port" : 8001,
+                  "protocol" : "TCP"
+                }
+              ],
+            },
+            {
+              "type" : "LoadBalancer",
+              "metadata" : {
+                "annotations" : {
+                  "kubernetes.io/elb.health-check-flag" : "on",
+                  "kubernetes.io/elb.health-check-option" : "{\"protocol\":\"TCP\",\"delay\":\"5\",\"timeout\":\"10\",\"max_retries\":\"3\"}"
+                },
+              },
+              "elb_id" : "ad0e9972-d330-4072-827b-095db7fa711c",
+              "access_control" : null,
+              "ports" : [
+                {
+                  "target_port" : 80,
+                  "port" : 8002,
+                  "protocol" : "TCP",
+                }
+              ],
+            },
+            {
+              "type" : "Ingress",
+              "metadata" : {
+                "annotations" : {
+                  "kubernetes.io/elb.health-check-flag" : "off",
+                  "kubernetes.io/elb.lb-algorithm" : "ROUND_ROBIN"
+                },
+              },
+              "elb_id" : "47bc918f-9ff1-465e-8910-a5154d9aeb0a",
+              "ports" : [
+                {
+                  "target_port" : 80,
+                  "port" : 449,
+                  "protocol" : "HTTPS",
+                  "default_certificate" : "pkcs88",
+                  "policy" : "tls-1-2-strict",
+                  "paths" : [
+                    {
+                      "hostname" : "huangtian.com",
+                      "path" : "/",
+                      "url_match_mode" : "STARTS_WITH",
+                    }
+                  ]
+                }
+              ],
+            },
+            {
+              "type": "Ingress",
+              "metadata": {
+                "annotations": {
+                  "kubernetes.io/elb.health-check-flag": "off",
+                  "kubernetes.io/elb.lb-algorithm": "ROUND_ROBIN"
+                }
+              },
+              "elb_id": "47bc918f-9ff1-465e-8910-a5154d9aeb0a",
+              "ports": [
+                {
+                  "target_port": 80,
+                  "port": 450,
+                  "protocol": "HTTP",
+                  "paths": [
+                    {
+                      "hostname": "hft.com"
+                      "path": "/",
+                      "url_match_mode": "STARTS_WITH",
+                    }
+                  ]
+                }
+              ],
+            }
+          ]
+        }
+      })
+    }
+  ]
+}
 resource "huaweicloud_cae_component" "test" {
   environment_id = "46dc4388-0383-4f16-b5d0-81bc7e28ad93"
   application_id =  "75e7e682-d7c9-4dde-b5c4-7cd954bbdecb"
